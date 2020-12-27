8098e7a915b81b5c775d3edbfbc2c4701841cf4c
HiepCongChua <hieplevuc@gmail.com>
2020-12-27 23:06:01 +0700

    git commit -m "added deployment scripts"

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
-docker build -t hieple/multi-client:latest -t hieple/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t hieple/multi-server:latest -t hieple/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t hieple/multi-worker:latest -t hieple/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push hieple/multi-client:latest
-docker push hieple/multi-server:latest
-docker push hieple/multi-worker:latest
+docker push stephengrider/multi-client:latest
+docker push stephengrider/multi-server:latest
+docker push stephengrider/multi-worker:latest
 
 kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
+kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
